70cc22585d881d5e831b71cdf31f1ec1f66c032d
peterstrzk <peterstrzk@gmail.com>
2022-09-23 10:07:37 +0200

    something with class

--- zipping.py
+++ zipping.py
@@ -14,31 +14,27 @@ class MyClass:
         #     print(k, v)
 
     #putting zipped stuff into dictionary
-    def zipped(c, self):
+    def zipped(self, c):
         result_c= dict(c)
         print(result_c)
         fin_mex = max(result_c, key=result_c.get)
         self.fin_mex = fin_mex
         return fin_mex
-        # print(f"{fin_max} wins!")
-        # lol = who_wins(fin_max)
-        # return lol
-        # for k, v in result_c.items():
-            # print(k, v)
-    # def who_wins(who):
 
-    #     print(f"{who} wins!")
-
+    def ending(self):
+        print(self.fin_mex)
     #looping with input condition
+
     shouldEnd = False
     while not shouldEnd:
         a = input("Type a name \n")
         b = int(input("Type value \n"))
         
         inputed_name = name_input(name = a, value = b)
-        zipped(c = inputed_name)
+
         restart = input("Want to restart? If you want type Y, if not type N.")
         if restart == "n":
-            # print(fin_mex)
+            ending()
+            
             shouldEnd = True
             ("goodbye!")
